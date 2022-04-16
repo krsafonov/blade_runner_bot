d9b53d6f98992f751fc1df8921d279598e4d7234
safonow <kirillsafonow@gmail.com>
2022-04-16 15:26:23 +0300

    change starting msg

--- handlers/users/reply_procces.py
+++ handlers/users/reply_procces.py
@@ -13,5 +13,4 @@ dp.message_handler()
 @dp.message_handler(Command("start"))  # made introduction and logic of collecting user's data
 async def show_items(message: Message):
     await message.answer(
-        text="Доброго времени суток, юзеры.\nДанный бот призван облегчить ваш процесс работы с посольством Чешской Республики в Москве \n"
-             "Для начала работы выберете топик снизу")
+        text="This Blade Runner Game by Kirill and Mikhail.")
